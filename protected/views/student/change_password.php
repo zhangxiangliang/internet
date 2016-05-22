e8a8e180e81c4da77c650c0c734cc71587f39f3d
zhangxiangliang <326277403@qq.com>
2016-05-22 23:41:04 +0800

    修改密码界面

--- protected/views/student/change_password.php
+++ protected/views/student/change_password.php
@@ -1,18 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php $baseUrl = Yii::app()->request->baseUrl;?>
 <head>
     <meta charset="UTF-8">
+    <!-- 设置宽度为当前设备宽度，缩放倍数为1，禁止用户缩放 -->
     <meta name="viewport" content="initial-scale=1.0, width=device-width, user-scalable=no">
-    <link rel="stylesheet" href="<?php echo $baseUrl;?>/css/internet.css">
-    <title>互联网 - 带你探索代码的世界</title>
+    <!-- 411er 引入后台专有样式 - internet-back.css -->
+    <link rel="stylesheet" href="/css/internet-back.css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <!-- 411er 填入当前页面名 -->
+    <title>互联网+</title>
 </head>
 <body>
-    <div class="header">
-        <div class="banner">
-            <h1 class="white">互联网+</h1>
-            <h3 class="white">修改密码</h3>
-            <div class="login">
+    <div id="content">
+        <!-- 411er 后台页面的头部 -->
+        <header id="header">
+            <div class="container">
+                <h1>互联网+</h1>
+                <h2>实践基地实训管理系统</h2>
+            </div>
+        </header>
+
+        <!-- 411er 后台页面的主体，包含导航 -->
+        <div class="container">
+            <!-- 小屏幕 导航按钮 -->
+            <a href="#nav" class="open toggle-btn"><i class="fa fa-reorder"></i></a>
+            <nav id="nav">
+                <ul>
+                    <li><a href="/Student/SearchCourses/">我的课程</a></li>
+                    <li><a href="/Student/PublicCoursesList/">选课</a></li>
+                    <!-- <li><a href="/Student/SearchTeacher/" class="active">教师查询</a></li> -->
+                    <li><a href="/Student/SearchScore/">成绩查询</a></li>
+                    <li><a href="/Student/index/" class="active">个人信息</a></li>
+                    <li><a href="/site/logout/">退出</a></li>
+                    <a href="#top" class="close toggle-btn"><i class="fa fa-remove"></i></a>
+                </ul>
+            </nav>
+            <!-- 表单 -->
+            <div class="table">
+                <h2>密码修改</h2>
                 <?php $form=$this->beginWidget('CActiveForm', array( 'id'=>'login-form', 'enableClientValidation'=>true, 'clientOptions'=>array( 'validateOnSubmit'=>true, ), )); ?>
                 <div class="form-group">
                     <?php echo $form->passwordField($model,'password',['placeholder'=>'旧密码']); ?>
@@ -29,9 +54,15 @@
                 <div class="btns">
                     <?php echo CHtml::submitButton( '提交',$htmlOptions=array ('class'=>'btn blue')); ?>
                 </div>
-                <?php $this->endWidget(); ?>          
+                <?php $this->endWidget(); ?>
             </div>
         </div>
     </div>
+    <!-- 411er 后台页面的尾部 -->
+    <footer id="footer" style="clear: both;">
+        <div class="container">
+            <h5>&copy; Copyright 互联网+ </h5>
+        </div>
+    </footer>
 </body>
 </html>
